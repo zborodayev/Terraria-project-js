f1cd97365d5f3bfb2f6a2d6fc719b6e6c5f03abc
Zzz <zakharb713@gmail.com>
2024-09-07 17:05:18 +0500

    12

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,7 +6,8 @@ export const Home = () => {
     <div className={styles.main}>
         <div>
             <h1 className="text-3xl flex justify-center" >Terraria class guide</h1>
-            <h1 className="text-2xl flex justify-center mt-3 " >There are <Link  className="mx-2 hover:text-slate-700 duration-150 " to={'/class'}>4 classes</Link> in Terraria</h1>
+            <h1 className="flex justify-center text-xl m-2" >This is my joint project with <img src="/manukek.jpeg" className="w-10 h-10 rounded-full m-1" /> manukek</h1>
+            <Link  className="flex justify-center text-xl mx-2 text-blue-500 hover:underline duration-150 " to={'/class'}>Guide</Link>
         </div>
     </div>
   )
--- src/components/screens/Classes.jsx
+++ src/components/screens/Classes.jsx
@@ -1,13 +1,11 @@
+import { Link } from 'react-router-dom'
 import styles from '../css/App.module.scss'
 export const Classes = () => {
   return (
     <div>
         <h1 className="flex mt-3 justify-center text-3xl" >There are 4 classes in Terraria</h1>
         <div className="flex justify-center m-2 " >
-            <h1 className={styles.classes} >melee</h1>
-            <h1 className={styles.classes} >ranged</h1>
-            <h1 className={styles.classes} >magic</h1>
-            <h1 className={styles.classes} >summoner</h1>
+            <h1 className={styles.classes}><Link className={styles.class} >melee</Link>, <Link className={styles.class} >ranged</Link>, <Link className={styles.class} >magic</Link> and <Link className={styles.class} >summoner</Link></h1>
         </div>
     </div>
   )
